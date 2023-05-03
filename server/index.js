746539b545178bc051e665a45e05ef69b0f12306
50_Abhishek Jani <88500027+abhishekjani08@users.noreply.github.com>
2023-05-04 01:21:04 +0530

    main

--- server/index.js
+++ server/index.js
@@ -111,11 +111,63 @@ app.post('/api/mediapipe/yog3', (req, res) => {
     }
   });
 });
+app.post('/api/mediapipe/yog4', (req, res) => {
+  const pythonProcess = spawn('python', ["main4.py"]);
 
-app.get('/get/mediapipe/yog4', (req, res) => {
-  res.send("hello world!!")
+  pythonProcess.stdout.on('data', (data) => {
+    console.log(data.toString());
+  });
+
+  pythonProcess.stderr.on('data', (data) => {
+    console.error(data.toString());
+  });
+
+  pythonProcess.on('close', (code) => {
+    if (code === 0) {
+      res.status(200).send('MediaPipe code executed successfully');
+    } else {
+      res.status(500).send(`MediaPipe code failed with error code `);
+    }
+  });
 });
+app.post('/api/mediapipe/yog5', (req, res) => {
+  const pythonProcess = spawn('python', ["main5.py"]);
 
+  pythonProcess.stdout.on('data', (data) => {
+    console.log(data.toString());
+  });
+
+  pythonProcess.stderr.on('data', (data) => {
+    console.error(data.toString());
+  });
+
+  pythonProcess.on('close', (code) => {
+    if (code === 0) {
+      res.status(200).send('MediaPipe code executed successfully');
+    } else {
+      res.status(500).send(`MediaPipe code failed with error code `);
+    }
+  });
+});
+app.post('/api/mediapipe/yog6', (req, res) => {
+  const pythonProcess = spawn('python', ["main6.py"]);
+
+  pythonProcess.stdout.on('data', (data) => {
+    console.log(data.toString());
+  });
+
+  pythonProcess.stderr.on('data', (data) => {
+    console.error(data.toString());
+  });
+
+  pythonProcess.on('close', (code) => {
+    if (code === 0) {
+      res.status(200).send('MediaPipe code executed successfully');
+    } else {
+      res.status(500).send(`MediaPipe code failed with error code `);
+    }
+  });
+});
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
 });
